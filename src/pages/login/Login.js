94e7132ac095dea6aa5f21a8bc2f944c8959f069
avincent6 <vincent6@purdue.edu>
2019-02-02 14:50:00 -0500

    create state and route login submit button to dashboard page

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import LoginForm from './LoginForm';
+import { Redirect } from 'react-router-dom';
 
-const Login
- = () => (
-  <div>
-    <p>Login Page</p>
-  </div>
-);
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      redirectToReferrer: false,
+      error: null
+    };
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    e.persist();
+    this.setState({redirectToReferrer: true, error: null});
+  };
+  render() {
+    if (this.state.redirectToReferrer === true) {
+      return (<Redirect to="/dashboard"/>);
+    }
+    return (
+      <div className="page bgorange">
+        <div className="logo">
+          simplif.ai
+        </div>
+        <h1>Login</h1>
+        <div className="registerbox">
+            <LoginForm login={this.handleSubmit} error={this.state.error} />
+        </div>
+      </div>
+    );
+  }
+}
+
 
 export default Login;
