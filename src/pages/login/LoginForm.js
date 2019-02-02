29e75c1325d96f6332702e37385e062a926b06ba
avincent6 <vincent6@purdue.edu>
2019-02-02 14:49:36 -0500

    Create LoginFrom and send handleSubmit down to the component

---
+++ src/pages/login/LoginForm.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const LoginForm = ({ login , error }) => (
+  <form onSubmit={login}>
+    <div className = "errorClass">
+      {error ? {error} : null}
+    </div>
+    <label htmlFor="email">email </label>
+    <input type="email" name="email" required />
+    <label htmlFor="password">password </label>
+    <input type="password" name="password" required />
+    <br/>
+    <input className="btn" type="submit" name="submit" value="submit" />
+    <a href="/request-password-reset">Forgot your password?</a>
+    <br/>
+    <a href="/register">Create an Account</a>
+  </form>
+);
+
+export default LoginForm;
