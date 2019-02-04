1f56e2d26e23eee5496465aa27710fd22bd41710
seanbecker15 <sean.becker15@gmail.com>
2019-02-04 17:34:01 -0500

    very basic, nasty implementation of login. Needs refactoring

--- src/pages/profile/Settings.js
+++ src/pages/profile/Settings.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import LoginForm from '../login/LoginForm';
+import { Redirect } from 'react-router-dom';
 
-const Settings = () => (
-  <div>
-    <p>Settings Page</p>
-  </div>
-);
+class Settings extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      redirectToReferrer: false,
+      error: null
+    };
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    e.persist();
+    this.setState({redirectToReferrer: true, error: null});
+  };
+
+  render() {
+    if (this.state.redirectToReferrer === true) {
+      return (<Redirect to="/dashboard"/>);
+    }
+    return (
+      <div className="page bgorange">
+        <div className="logo">
+          simplif.ai
+        </div>
+        <h1>Login</h1>
+        <div className="registerbox">
+            <LoginForm login={this.handleSubmit} error={this.state.error} />
+        </div>
+      </div>
+    );
+  }
+}
+
+// const Settings = () => (
+//   <div>
+//     <p>Settings Page</p>
+//     <form>
+//       <input name="name" type="text" />
+//     </form>
+//   </div>
+// );
 
 export default Settings;
