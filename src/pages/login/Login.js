b1fe9f8d57ca67d1f42a6a7019237d00818b301a
seanbecker15 <sean.becker15@gmail.com>
2019-02-20 17:02:37 -0500

    first login updates user profile and routes to home page when info updated

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import LoginForm from './LoginForm';
 import FactorForm from './FactorForm';
+import FirstLoginForm from './FirstLoginForm';
 import { Redirect } from 'react-router-dom';
-import apiFetch from '../../utils/api';
+import { apiFetch, apiPost } from '../../utils/api';
 import { saveToLocalStorage, deleteFromLocalStorage } from '../../utils/localstorage';
+import { getAttributesFromEvent } from '../../utils/forms';
 import '../../assets/login.css';
 import logo from '../../assets/images/logo.png';
 
@@ -16,6 +18,7 @@ class Login extends Component {
       codeSent: false,
       username: null,
       password: null,
+      firstLogin: null
     };
   }
 
@@ -23,17 +26,47 @@ class Login extends Component {
     e.preventDefault();
     e.persist();
     this.setState({error: null});
-    console.log(`username: ${e.target.username.value}`)
-    console.log(`password: ${e.target.password.value}`)
     this.setState({password: e.target.password.value});
+    this.setState({username: e.target.username.value});
     const formData = {
-      body: JSON.stringify({
-        "username": e.target.username.value,
-        "password": e.target.password.value
-      }),
-      method: 'POST'
+      username: e.target.username.value,
+      password: e.target.password.value
     }
-    await apiFetch('/auth/login/', formData)
+    const res = await apiPost('/auth/login/', formData, false);
+    console.log(res);
+    const {data} = res;
+    console.log(data);
+    switch(res.status) {
+      case 200:
+        if (data.message === 'code sent') {
+          this.setState({codeSent: true});
+          return;
+        } else if (data.message === 'first login') {
+          if (!data.token) {
+            console.log('A token should have been sent')
+          }
+          saveToLocalStorage({token: data.token});
+          this.setState({firstLogin: true});
+        } else {
+          // error unknown response
+        }
+        break;
+      // invalid token
+      case 401:
+        deleteFromLocalStorage('token');
+        break;
+      // TODO invalid user/pass
+      case 404:
+        break;
+      // TODO server error
+      case 500:
+        console.log('server error');
+        // fallthrough
+      default:
+        console.log(`Responded with status ${res.status}`);
+
+    }
+    /*await apiFetch('/auth/login/', formData)
       .then(res => {
         return res.json().then(data => {
           // handle 2-factor
@@ -49,7 +82,7 @@ class Login extends Component {
             deleteFromLocalStorage('token');
           }
         })
-    });
+    });*/
   };
 
   handleFactorSubmit = async (e) => {
@@ -78,6 +111,37 @@ class Login extends Component {
     });
   }
 
+  handleFirstLogin = async (e) => {
+    e.preventDefault();
+    e.persist();
+    this.setState({error: null});
+    const attributes = ['first_name', 'last_name', 'phone_number', 'new_password']
+    const formData = getAttributesFromEvent(attributes, e);
+    formData.username = this.state.username;
+    formData.password = this.state.password;
+
+    const res = await apiPost('/auth/firstlogin/', formData, true);
+    const {data} = res;
+
+    // TODO handle response
+    switch(res.status) {
+      case 200:
+        if (data.message === 'profile updated.') {
+          this.setState({redirectToReferrer: true})
+        } else {
+          console.log(data)
+          //this.setState({error: 'Something went wrong updating your profile. Check log for more info.'});
+          console.log(`HTTP Status (for error): ${res.status}`)
+          console.log(`HTTP Status Text (for error): ${res.statusText}`)
+        }
+        break;
+      default:
+        // this.setState({ error: 'Something went wrong updating your profile. Check log for more info.'});
+        console.log(`HTTP Status (for error): ${res.status}`);
+        console.log(`HTTP Status Text (for error): ${res.statusText}`);
+    }
+
+  }
   render() {
     if (this.state.redirectToReferrer === true) {
       return (<Redirect to="/dashboard"/>);
@@ -91,8 +155,9 @@ class Login extends Component {
               </h1>
               {/* login form disappears when codeSent is true */}
               {/* 2-factor form appears when codeSent is true */}
-              {!this.state.codeSent && <LoginForm login={this.handleSubmit} error={this.state.error} />}
-              {this.state.codeSent && <FactorForm login={this.handleFactorSubmit} error={this.state.error} />}
+              {!this.state.firstLogin && !this.state.codeSent && <LoginForm login={this.handleSubmit} error={this.state.error} />}
+              {!this.state.firstLogin && this.state.codeSent && <FactorForm login={this.handleFactorSubmit} error={this.state.error} />}
+              {this.state.firstLogin && <FirstLoginForm updateProfile={this.handleFirstLogin} error={this.state.error} />}
             </div>
         </div>
         <div className="right">
