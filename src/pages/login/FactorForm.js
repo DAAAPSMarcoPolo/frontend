f44d10ea9e219dfff6480f080146871de0cf2f5a
avincent6 <vincent6@purdue.edu>
2019-02-20 16:12:43 -0500

    update styling and make arrow a button

--- src/pages/login/FactorForm.js
+++ src/pages/login/FactorForm.js
@@ -8,7 +8,7 @@ const FactorForm = ({login, error}) => (
         </div>
         <div>
           <input type="text" name="code" placeholder="Code" required/>
-          <img className="submit-arrow" src={arrow} alt="arrow" />
+          <button className="btn-img"><img className="submit-arrow" src={arrow} alt="arrow" /></button>
         </div>
         <button className="submit-btn"  type="submit" name="submit" value="submit">Submit</button>
         <a href="/request-password-reset">Didn't receive a code?</a>
