7695d29fdc6c098d4f0f238e19678aa38a8c2ed5
avincent6 <vincent6@purdue.edu>
2019-02-19 23:56:02 -0500

    style verif code page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './Routes';
 import Nav from './pages/components/Nav';
--- src/pages/login/FactorForm.js
+++ src/pages/login/FactorForm.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import arrow from '../../assets/images/arrow.png';
 
 const FactorForm = ({login, error}) => (
     <form onSubmit={login}>
         <div className="errorClass">
             {error ? {error} : null}
         </div>
-        <label htmlFor="code">Code </label>
-        <input type="text" name="code" required/>
-        <br/>
-        <input className="btn" type="submit" name="submit" value="submit"/>
+        <div>
+          <input type="text" name="code" placeholder="Code" required/>
+          <img className="submit-arrow" src={arrow} alt="arrow" />
+        </div>
+        <button className="submit-btn"  type="submit" name="submit" value="submit">Submit</button>
+        <a href="/request-password-reset">Didn't receive a code?</a>
     </form>
 );
 
