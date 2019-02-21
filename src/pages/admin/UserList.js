d2077f78fa9d05db5c076a643aea3fc453da3833
avincent6 <vincent6@purdue.edu>
2019-02-21 14:56:41 -0500

    refactor and add styling to user list page

--- src/pages/admin/UserList.js
+++ src/pages/admin/UserList.js
@@ -1,70 +1,38 @@
 import React, {Component} from 'react';
+import plus from '../../assets/images/plus-icon.png';
+import remove from '../../assets/images/delete-icon.png';
 
 class UserList extends Component {
     constructor(props){
         super(props);
         this.state = {
-            redirectToReferrer: false,
             error: null,
-            currentConfirmation:""
+            currentConfirmation: ""
         };
         this.confirmUser = this.confirmUser.bind(this);
     };
-
     confirmUser(e, username){
         console.log("Confirming user: " + username);
         this.setState({currentConfirmation:username});
     };
-
-    userOption(user){
-        let removeUserButton;
-        if (this.state.currentConfirmation === user){
-            removeUserButton = (
-                <div>{user}
-                    <button onClick={(e) => this.props.removeUser(e, user)}>Confirm Removing: {user}</button>
-                </div>
-            )
-        } else {
-            removeUserButton = (
-                <div>{user}
-                    <button onClick={(e) => this.confirmUser(e, user)}>Remove User: {user}</button>
-                </div>
-            )
-        }
-        return removeUserButton;
-    }
-
-    userList() {
-        const mappedUsers = this.props.users.map((user) =>
-            <li>
-                <div>
-                    {this.userOption(user)}
-                </div>
-            </li>
-        );
-        return mappedUsers;
-    }
-
     render() {
-        return this.userList()
+        const mappedUsers = this.props.users.map((user) =>
+          <li className="li">
+            <div>
+              {this.state.currentConfirmation === user ?
+                <div className="rel">{user}
+                    <img className="icon" src={plus} alt="plus-icon" onClick={(e) => this.props.removeUser(e, user)}/>
+                </div>
+              :
+              <div className="rel">{user}
+                  <img className="icon" src={remove} onClick={(e) => this.confirmUser(e, user)}/>
+              </div>
+              }
+            </div>
+          </li>
+      );
+      return mappedUsers;
     }
 };
-
-/*const UserList = ({users, removeUser}) => {
-    const userList = users.map((user) =>
-        <li>
-            <div>
-                {user}<button onClick={(e) => removeUser(e, user)}>Remove User: {user}</button>
-            </div>
-        </li>
-    );
-    return (
-        <div>
-            <ul>
-                {userList}
-            </ul>
-        </div>
-    );
-};*/
 
 export default UserList;
