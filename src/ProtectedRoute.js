48a17a5a94cc03528a53fb1c5644236ba4052f2f
avincent6 <vincent6@purdue.edu>
2019-02-21 14:59:01 -0500

    add a route that redirects if cookie does not indicate logged in

---
+++ src/ProtectedRoute.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router';
+import { Redirect } from 'react-router-dom';
+import { withCookies, Cookies } from 'react-cookie';
+import { instanceOf } from 'prop-types';
+
+class ProtectedRoute extends Component {
+  static propTypes = {
+    cookies: instanceOf(Cookies).isRequired
+  };
+  render() {
+    const { component: Component, cookies, ...props } = this.props;
+    const isAuthenticated = cookies.get('isAuthenticated');
+    console.log(isAuthenticated);
+    return (
+      <Route
+      {...props}
+      render={props => (
+        (!isAuthenticated) ?
+        <Redirect to='/login' /> :
+        <Component {...props} />
+      )}
+      />
+    )
+  }
+}
+
+export default withCookies(ProtectedRoute);
