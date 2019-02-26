c65d01073a46cdab758f5c78dc901b2e38e65161
greenguy21 <mountainowlbiker@gmail.com>
2019-02-26 16:42:09 -0500

    Reformatted the Settings page (making sure everything is tabbed over properly)

--- src/pages/admin/Settings.js
+++ src/pages/admin/Settings.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import AlpacaPreferencesForm from './AlpacaPreferencesForm'
 import {Redirect} from 'react-router-dom';
 import {apiFetch, apiPost, apiGet, apiDelete} from '../../utils/api';
-import { withCookies } from 'react-cookie';
+import {withCookies} from 'react-cookie';
 import api from '../../utils/apiv2';
 import './admin.css';
 import x from '../../assets/images/x-icon.png';
@@ -34,12 +34,13 @@ class Settings extends Component {
         const response = await api.Get('/users/list/')
         this.setState({userslist: response.data.users});
     };
+
     handleRemoveUser = async (e, username) => {
         console.log("Removing a user:", username);
         const formbody = {
             username: username
         };
-        const response =  await apiDelete('/users/list/', formbody);
+        const response = await apiDelete('/users/list/', formbody);
         console.log(response);
     };
 
@@ -55,57 +56,60 @@ class Settings extends Component {
         console.log(response.status);
     };
 
-  handleSubmitAlpacaKey = async (e) => {
-    e.preventDefault();
-    e.persist();
-    console.log(`new key: ${e.target.key_id.value}`);
-    const formData = {
-        body: JSON.stringify({
-            "key_id" : e.target.key_id.value,
-            "secret_key" : e.target.secret_key.value,
-            "user": 1
-        }),
-        method: 'POST'
+    handleSubmitAlpacaKey = async (e) => {
+        e.preventDefault();
+        e.persist();
+        console.log(`new key: ${e.target.key_id.value}`);
+        const formData = {
+            body: JSON.stringify({
+                "key_id": e.target.key_id.value,
+                "secret_key": e.target.secret_key.value,
+                "user": 1
+            }),
+            method: 'POST'
+        };
+        const data = await apiFetch('/alpaca/', formData);
+        console.log('data', data.status);
     };
-    const data = await apiFetch('/alpaca/', formData);
-    console.log('data', data.status);
-  };
-  showAlpacaPreferences = () => {
-      this.setState({showAlpaca: !this.state.showAlpaca})
-  };
-  render() {
-    const { cookies } = this.props;
-    const isAuthenticated = cookies.get('isAuthenticated');
-    const isAdmin = cookies.get('isAdmin');
-    if (isAuthenticated === "false" || !isAuthenticated) {
-      return (<Redirect to="/login"/>);
-    }
+    showAlpacaPreferences = () => {
+        this.setState({showAlpaca: !this.state.showAlpaca})
+    };
 
-    let alpacaApiSettings;
-    if (this.state.showAlpaca === false) {
-        alpacaApiSettings = <button id="add-user-hide-button" onClick={this.showAlpacaPreferences}>Modify Alpaca Preferences</button>
-    } else if (this.state.showAlpaca === true) {
-        alpacaApiSettings = (
-            <div className="con rel">
-                <img src={x} className="icon roster" alt="x-icon" onClick={this.showAlpacaPreferences}/>
-                <AlpacaPreferencesForm updateAlpacaKey={this.handleSubmitAlpacaKey}/>
+    render() {
+        const {cookies} = this.props;
+        const isAuthenticated = cookies.get('isAuthenticated');
+        const isAdmin = cookies.get('isAdmin');
+        if (isAuthenticated === "false" || !isAuthenticated) {
+            return (<Redirect to="/login"/>);
+        }
+
+        let alpacaApiSettings;
+        if (this.state.showAlpaca === false) {
+            alpacaApiSettings = <button id="add-user-hide-button" onClick={this.showAlpacaPreferences}>Modify Alpaca
+                Preferences</button>
+        } else if (this.state.showAlpaca === true) {
+            alpacaApiSettings = (
+                <div className="con rel">
+                    <img src={x} className="icon roster" alt="x-icon" onClick={this.showAlpacaPreferences}/>
+                    <AlpacaPreferencesForm updateAlpacaKey={this.handleSubmitAlpacaKey}/>
+                </div>
+            )
+        }
+
+        return (
+            <div className="page temptext">
+                <h1>Settings Page</h1>
+                <Profile/>
+                <br/><br/>
+                <UserList users={this.state.userslist} removeUser={this.handleRemoveUser} isAdmin={isAdmin}
+                          error={this.state.error} getUsersList={this.getUsersList}/>
+                <br/>
+                <div className="con">
+                    {alpacaApiSettings}
+                </div>
             </div>
-        )
+        );
     }
-
-    return (
-      <div className="page temptext">
-          <h1>Settings Page</h1>
-          <Profile />
-          <br/><br/>
-          <UserList users={this.state.userslist} removeUser={this.handleRemoveUser} isAdmin={isAdmin} error={this.state.error} getUsersList={this.getUsersList} />
-          <br/>
-          <div className="con">
-          {alpacaApiSettings}
-          </div>
-      </div>
-    );
-  }
 }
 
 export default withCookies(Settings);
