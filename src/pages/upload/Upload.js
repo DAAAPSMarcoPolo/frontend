cbbcf8f6f47f1c955b2275ddb2945b7a77326e8d
greenguy21 <mountainowlbiker@gmail.com>
2019-02-28 13:17:16 -0500

    Added some styling and button functionality to uploading a python file. Everything here works. Waiting for API calls to be functional.

--- src/pages/upload/Upload.js
+++ src/pages/upload/Upload.js
@@ -1,21 +1,50 @@
-import {React, Component} from 'react'
+//Module Dependencies
+import React, {Component} from 'react'
 import {Redirect} from "react-router-dom";
+import {withCookies} from 'react-cookie';
+
+//Custom Dependencies
+import FileSelection from './FileSelection';
+import '../../assets/upload.css';
+import '../../App.css';
 
 class Upload extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            files: null
-        }
-    }
+            files: null,
+            filename: null,
+            showUploadButton: false
+        };
+        this.handleFileSelection = this.handleFileSelection.bind(this);
+        this.handleFileUpload = this.handleFileUpload.bind(this);
+        this.showUploadButton = this.showUploadButton.bind(this);
 
-    async handleFileSelection() {
-        // TODO: modify state to handle uploaded file
-    }
+    };
 
-    async handleFileUpload() {
+    async handleFileSelection(e) {
+        e.preventDefault();
+        e.persist();
+        const uploadedFile = e.target.files[0];
+        console.log(uploadedFile);
+        this.setState({files: uploadedFile, filename: uploadedFile.name});
+        this.showUploadButton();
+    };
+
+    async handleFileUpload(e) {
+        e.preventDefault();
+        e.persist();
         // TODO: make API call to upload file to backend
-    }
+        if (this.state.files != null) {
+            console.log("PLACEHOLDER: Sending file to backend");
+        } else {
+            console.log("No file chosen to upload");
+        }
+    };
+
+    showUploadButton = () => {
+        this.setState({showUploadButton: !this.state.showUploadButton});
+    };
 
     render() {
         const {cookies} = this.props;
@@ -26,13 +55,24 @@ class Upload extends Component {
         //     return (<Redirect to="/login"/>);
         // }
 
+        let uploadButton;
+        if (this.state.showUploadButton === true) {
+            uploadButton = <button id="upload-button" onClick={this.handleFileUpload}>Upload Algorithm</button>
+        } else {
+            uploadButton = <button id="upload-button" onClick={this.handleFileUpload} disabled>Upload Algorithm</button>
+        }
+
         return (
-            <div>
-                <div>Placeholder for file uploader</div>
-                <div>Placeholder for status checking</div>
+            <div className="algo-upload-con">
+                <FileSelection
+                    handleFileSelection={this.handleFileSelection}
+                    handleFileUpload={this.handleFileUpload}
+                    filename={this.state.filename}
+                />
+                {uploadButton}
             </div>
-        )
+        );
     }
 }
 
-export default Upload;
+export default withCookies(Upload);
