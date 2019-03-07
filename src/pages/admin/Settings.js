4081d596001b1c453bac559a11d6579c1d1d6634
seanbecker15 <sean.becker15@gmail.com>
2019-03-07 15:44:16 -0500

    merging

--- src/pages/admin/Settings.js
+++ src/pages/admin/Settings.js
@@ -1,8 +1,8 @@
-import React, {Component} from 'react';
-import AlpacaPreferencesForm from './AlpacaPreferencesForm'
-import {Redirect} from 'react-router-dom';
+import React, { Component } from 'react';
+import AlpacaPreferencesForm from './AlpacaPreferencesForm';
+import { Redirect } from 'react-router-dom';
 import api from '../../utils/api';
-import {withCookies} from 'react-cookie';
+import { withCookies } from 'react-cookie';
 import './admin.css';
 import x from '../../assets/images/x-icon.png';
 import UserList from './UserList';
@@ -18,7 +18,8 @@ class Settings extends Component {
             username: '',
             first_name: '',
             last_name: '',
-            password: ''
+            password: '',
+            is_staff: null
         };
         this.showAlpacaPreferences = this.showAlpacaPreferences.bind(this);
         this.getUsersList = this.getUsersList.bind(this);
@@ -30,11 +31,11 @@ class Settings extends Component {
 
     async getUsersList() {
         const response = await api.Get('/users/list/');
-        this.setState({userslist: response.data.users});
-    };
+        this.setState({ userslist: response.data.users });
+    }
 
     handleRemoveUser = async (e, username) => {
-        console.log("Removing a user:", username);
+        console.log('Removing a user:', username);
         const formbody = {
             username: username
         };
@@ -42,53 +43,70 @@ class Settings extends Component {
         console.log(response);
     };
 
-    handleSubmitAlpacaKey = async (e) => {
+    handleSubmitAlpacaKey = async e => {
         e.preventDefault();
         e.persist();
         let formData = {
-            "key_id": e.target.key_id.value,
-            "secret_key": e.target.secret_key.value
+            key_id: e.target.key_id.value,
+            secret_key: e.target.secret_key.value
         };
         const response = await api.Post('/alpaca/', formData);
         console.log(response.status);
     };
 
     showAlpacaPreferences = () => {
-        this.setState({showAlpaca: !this.state.showAlpaca})
+        this.setState({ showAlpaca: !this.state.showAlpaca });
     };
 
     render() {
-        const {cookies} = this.props;
+        const { cookies } = this.props;
         const isAuthenticated = cookies.get('isAuthenticated');
         const isAdmin = cookies.get('isAdmin');
-        if (isAuthenticated === "false" || !isAuthenticated) {
-            return (<Redirect to="/login"/>);
+        if (isAuthenticated === 'false' || !isAuthenticated) {
+            return <Redirect to="/login" />;
         }
 
         let alpacaApiSettings;
         if (this.state.showAlpaca === false) {
-            alpacaApiSettings = <button id="add-user-hide-button" onClick={this.showAlpacaPreferences}>Modify Alpaca
-                Preferences</button>
+            alpacaApiSettings = (
+                <button
+                    id="add-user-hide-button"
+                    onClick={this.showAlpacaPreferences}
+                >
+                    Modify Alpaca Preferences
+                </button>
+            );
         } else if (this.state.showAlpaca === true) {
             alpacaApiSettings = (
                 <div className="con rel">
-                    <img src={x} className="icon roster" alt="x-icon" onClick={this.showAlpacaPreferences}/>
-                    <AlpacaPreferencesForm updateAlpacaKey={this.handleSubmitAlpacaKey}/>
+                    <img
+                        src={x}
+                        className="icon roster"
+                        alt="x-icon"
+                        onClick={this.showAlpacaPreferences}
+                    />
+                    <AlpacaPreferencesForm
+                        updateAlpacaKey={this.handleSubmitAlpacaKey}
+                    />
                 </div>
-            )
+            );
         }
 
         return (
             <div className="page temptext">
                 <h1>Settings Page</h1>
-                <Profile/>
-                <br/><br/>
-                <UserList users={this.state.userslist} removeUser={this.handleRemoveUser} isAdmin={isAdmin}
-                          error={this.state.error} getUsersList={this.getUsersList}/>
-                <br/>
-                <div className="con">
-                    {alpacaApiSettings}
-                </div>
+                <Profile />
+                <br />
+                <br />
+                <UserList
+                    users={this.state.userslist}
+                    removeUser={this.handleRemoveUser}
+                    isAdmin={isAdmin}
+                    error={this.state.error}
+                    getUsersList={this.getUsersList}
+                />
+                <br />
+                <div className="con">{alpacaApiSettings}</div>
             </div>
         );
     }
--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import api from '../../utils/api';
 import EditProfile from './EditProfile';
 import edit from '../../assets/images/edit-icon.png';
@@ -11,10 +11,12 @@ class Settings extends Component {
         this.state = {
             error: null,
             showEditProfile: false,
-            username: 'maudrey333@gmail.com',
-            first_name: 'Audrey',
-            last_name: 'Vincent',
-            phone_number: '8124706350',
+            user: {
+                username: '',
+                first_name: '',
+                last_name: '',
+                profile__phone_number: ''
+            },
             showConfirm: false,
             file: '',
             imagePreviewUrl: '',
@@ -29,12 +31,14 @@ class Settings extends Component {
         console.log(response);
     }
 
-    componentDidMount() {
+    async componentDidMount() {
         const formData = {
-            "username": this.state.username
-        }
-        console.log("componentDidMount");
-        this.getProfilePicture();
+            username: this.state.username
+        };
+        //this.getProfilePicture();
+        const res = await api.Get('/api/user/settings/');
+        const { user } = res.data;
+        this.setState({ user });
         // api.Get('/profilepicture/')
         //   .then(res => {
         //     return res.json().then(data => {
@@ -56,75 +60,79 @@ class Settings extends Component {
         // });
     }
 
-    editProfile = (e) => {
+    editProfile = e => {
         e.preventDefault();
         e.persist();
-        this.setState({error: null});
+        this.setState({ error: null });
         const formData = {
-            "username": e.target.username.value,
-            "first_name": e.target.first_name.value,
-            "last_name": e.target.last_name.value,
-            "phone_number": e.target.phone_number.value
-        }
-        api.Post('/user/settings/', formData)
-            .then(res => {
-                return res.json().then(data => {
-                    if (res.status === 200 && data.token) {
-                        // the response returned a success
-                        console.log('/user/settings/', 'success')
-                        this.setState({showConfirm: !this.state.showConfirm});
-                    } else if (res.status === 401) {
-                        if (res.message) {
-                            this.setState({error: res.message});
-                        }
+            username: e.target.username.value,
+            first_name: e.target.first_name.value,
+            last_name: e.target.last_name.value,
+            phone_number: e.target.phone_number.value
+        };
+        api.Post('/user/settings/', formData).then(res => {
+            return res.json().then(data => {
+                if (res.status === 200 && data.token) {
+                    // the response returned a success
+                    console.log('/user/settings/', 'success');
+                    this.setState({ showConfirm: !this.state.showConfirm });
+                } else if (res.status === 401) {
+                    if (res.message) {
+                        this.setState({ error: res.message });
                     }
-                })
+                }
             });
-    }
-    updatePassword = (e) => {
+        });
+    };
+    updatePassword = e => {
         e.preventDefault();
         e.persist();
-        this.setState({error: null});
+        this.setState({ error: null });
         const formData = {
-            "password": e.target.old_password.value,
-            "new_password": e.target.new_password.value
-        }
-        api.Put('/user/settings/', formData)
-            .then(res => {
-                return res.json().then(data => {
-                    if (res.status === 200 && data.token) {
-                        // the response returned a success
-                        console.log('/user/settings/', 'success')
-                        this.setState({showConfirm: !this.state.showConfirm});
-                    } else if (res.status === 401) {
-                        if (res.message) {
-                            this.setState({error: res.message});
-                        }
+            password: e.target.old_password.value,
+            new_password: e.target.new_password.value
+        };
+        api.Put('/user/settings/', formData).then(res => {
+            return res.json().then(data => {
+                if (res.status === 200 && data.token) {
+                    // the response returned a success
+                    console.log('/user/settings/', 'success');
+                    this.setState({ showConfirm: !this.state.showConfirm });
+                } else if (res.status === 401) {
+                    if (res.message) {
+                        this.setState({ error: res.message });
                     }
-                })
+                }
             });
-    }
-    savePicture = (e) => {
+        });
+    };
+    savePicture = e => {
         e.preventDefault();
         // TODO fix formData below (ask Sean bout dis)
         const formData = new FormData();
         console.log('pic', this.state.file);
-        formData.append("avatar", this.state.file);
+        formData.append('avatar', this.state.file);
         api.Put('/profilepicture/', formData).then(res => {
             return res.json().then(data => {
-                console.log('data', data)
+                console.log('data', data);
                 if (res.status === 200 && data.token) {
                     // the response returned a success
                     const url = window.URL.createObjectURL(data);
-                    console.log('/profilepicture/', 'success', data, 'url', url)
-                    this.setState({imagePreviewUrl: url});
+                    console.log(
+                        '/profilepicture/',
+                        'success',
+                        data,
+                        'url',
+                        url
+                    );
+                    this.setState({ imagePreviewUrl: url });
                     // this.setState({showConfirm: !this.state.showConfirm});
                 } else if (res.status === 401) {
                     if (res.message) {
-                        this.setState({error: res.message});
+                        this.setState({ error: res.message });
                     }
                 }
-            })
+            });
         });
         //
         // api.Get('/profilepicture/')
@@ -144,8 +152,8 @@ class Settings extends Component {
         //       }
         //     })
         // });
-    }
-    handleImageChange = (e) => {
+    };
+    handleImageChange = e => {
         e.preventDefault();
 
         let reader = new FileReader();
@@ -156,65 +164,106 @@ class Settings extends Component {
                 file: file,
                 imagePreviewUrl: reader.result
             });
-        }
-        reader.readAsDataURL(file)
-    }
+        };
+        reader.readAsDataURL(file);
+    };
     showEditProfile = () => {
-        this.setState({showEditProfile: !this.state.showEditProfile});
+        this.setState({ showEditProfile: !this.state.showEditProfile });
     };
     showUploadImage = () => {
-        this.setState({showUpload: !this.state.showUpload});
+        this.setState({ showUpload: !this.state.showUpload });
     };
     showEditPassword = () => {
-        this.setState({showPass: !this.state.showPass});
-    }
+        this.setState({ showPass: !this.state.showPass });
+    };
 
     render() {
+        const { user } = this.state;
         return (
             <div className="con rel">
                 <div className="profile-circle">
-                    <img src={this.state.imagePreviewUrl} alt="profile-pic" className="profile-img"/>
+                    <img
+                        src={this.state.imagePreviewUrl}
+                        alt="profile-pic"
+                        className="profile-img"
+                    />
                 </div>
                 <h2 className="serif">My Profile</h2>
                 {this.state.showEditProfile ? (
                     <div>
-                        <img className="icon roster" src={x} alt="x-icon" onClick={this.showEditProfile}/>
-                        <EditProfile submit={this.editProfile} error={this.state.error}
-                                     phone_number={this.state.phone_number} username={this.state.username}
-                                     first_name={this.state.first_name} last_name={this.state.last_name}/>
+                        <img
+                            className="icon roster"
+                            src={x}
+                            alt="x-icon"
+                            onClick={this.showEditProfile}
+                        />
+                        <EditProfile
+                            submit={this.editProfile}
+                            error={this.state.error}
+                            phone_number={user.profile__phone_number}
+                            username={user.username}
+                            first_name={user.first_name}
+                            last_name={user.last_name}
+                        />
                     </div>
                 ) : (
                     <div>
-                        <img className="icon roster" src={edit} alt="edit-icon" onClick={this.showEditProfile}/>
+                        <img
+                            className="icon roster"
+                            src={edit}
+                            alt="edit-icon"
+                            onClick={this.showEditProfile}
+                        />
                         <div className="profile">
-                            <h3>{this.state.username}</h3>
-                            <h4>{this.state.first_name} {this.state.last_name}</h4>
-                            <p>{this.state.phone_number}</p>
-                            {!this.state.showPass ?
-                                <button onClick={this.showEditPassword}>Update Password</button> : null}
+                            <h3>{user.username}</h3>
+                            <h4>
+                                {user.first_name} {user.last_name}
+                            </h4>
+                            <p>{user.profile__phone_number}</p>
+                            {!this.state.showPass ? (
+                                <button onClick={this.showEditPassword}>
+                                    Update Password
+                                </button>
+                            ) : null}
                         </div>
                     </div>
                 )}
-                {this.state.showUpload ?
+                {this.state.showUpload ? (
                     <form className="image-upload" onSubmit={this.savePicture}>
                         <h1>Profile Options</h1>
-                        <input className="fileInput"
-                               type="file"
-                               onChange={this.handleImageChange} required/>
-                        <button className="submitButton" type="submit">Upload New Profile Image</button>
+                        <input
+                            className="fileInput"
+                            type="file"
+                            onChange={this.handleImageChange}
+                            required
+                        />
+                        <button className="submitButton" type="submit">
+                            Upload New Profile Image
+                        </button>
                     </form>
-                    :
+                ) : (
                     <button onClick={this.showUploadImage}>Upload image</button>
-                }
-                {this.state.showPass ?
+                )}
+                {this.state.showPass ? (
                     <form onSubmit={this.updatePassword}>
                         <h3>Update Password</h3>
-                        <input type="text" name="old_password" placeholder="Old Password" required/>
-                        <input type="text" name="new_password" placeholder="New Password" required/>
-                        <button className="submitButton" type="submit">Update Password</button>
+                        <input
+                            type="text"
+                            name="old_password"
+                            placeholder="Old Password"
+                            required
+                        />
+                        <input
+                            type="text"
+                            name="new_password"
+                            placeholder="New Password"
+                            required
+                        />
+                        <button className="submitButton" type="submit">
+                            Update Password
+                        </button>
                     </form>
-                    :
-                    null}
+                ) : null}
             </div>
         );
     }
