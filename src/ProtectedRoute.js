b8ea298dfb16778f3a3fa8a60bfd9f1dce8966f9
avincent6 <vincent6@purdue.edu>
2019-03-23 15:22:39 -0400

    update protected route

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -6,43 +6,29 @@ import User from './pages/components/User';
 import {withCookies, Cookies} from 'react-cookie';
 import {instanceOf} from 'prop-types';
 
-class ProtectedRoute extends Component {
-  // = ({component: Component, cookies, ...props}) =>
-  constructor(props) {
-      super(props);
-      this.state = {
-        profilePic: null
-      };
-  }
-  // ComponentDidMount() {
-  //     // pull list of algorithms
-  // }
-  render() {
-    const cookies = this.props;
-    const props = this.props;
-    const isAuthenticated = cookies.get('isAuthenticated');
-    const isAdmin = cookies.get('isAdmin');
-    return (
-        <Route
-            {...props}
-            render={props => {
-                if (isAuthenticated === "true") {
-                  return (
-                    <div className="navwrap">
-                      <Nav isAuthenticated={isAuthenticated} isAdmin={isAdmin}/>
-                      <User isAuthenticated={isAuthenticated}/>
-                      <div className="wrapright mx-auto mt-3 page">
-                        <Component {...props} />
-                      </div>
+const ProtectedRoute = ({component: Component, cookies, ...props}) => {
+  const isAuthenticated = cookies.get('isAuthenticated');
+  const isAdmin = cookies.get('isAdmin');
+  return (
+      <Route
+          {...props}
+          render={props => {
+              if (isAuthenticated === "true") {
+                return (
+                  <div className="navwrap">
+                    <Nav isAuthenticated={isAuthenticated} isAdmin={isAdmin}/>
+                    <User isAuthenticated={isAuthenticated}/>
+                    <div className="wrapright mx-auto mt-3 page">
+                      <Component {...props} />
                     </div>
-                  );
-                } else {
-                  return ( <Redirect to='/login'/> );
-                }
-            }}
-        />
-    );
-  }
+                  </div>
+                );
+              } else {
+                return ( <Redirect to='/login'/> );
+              }
+          }}
+      />
+  );
 }
 
 export default withCookies(ProtectedRoute);
