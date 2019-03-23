969bef7ac86f12b056383b556bd7e70c877e846d
avincent6 <vincent6@purdue.edu>
2019-03-23 15:21:31 -0400

    udate user to pull profile pic

--- src/pages/components/User.js
+++ src/pages/components/User.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
-import profile from '../../assets/images/profile.jpg';
+import api from '../../utils/api';
 
-const User = ({isAuthenticated}) => {
-    if (isAuthenticated === "true") {
-        return (
-            <Link className="user" to='/settings'>
-                <div className="profile-nav">
-                    <img className="profile-pic" src={profile} alt="profile-pic"/>
-                </div>
-                <p>Username</p>
-            </Link>
-        );
-    } else return null;
-};
+class User extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        file: '',
+        imagePreviewUrl: '',
+      }
+  }
+  componentDidMount() {
+    this.getProfilePicture();
+  }
+  async getProfilePicture() {
+    const response = await api.Get('/profilepicture/');
+    console.log("response", response);
+    if (response.status === 200) {
+      // this.setState({ imagePreviewUrl: response.profile.avatar.url });
+      console.log("success!");
+    }
+  }
+  render() {
+      if (this.props.isAuthenticated === "true") {
+          return (
+              <Link className="user" to='/settings'>
+                  <div className="profile-nav">
+                      <img className="profile-pic" src={this.state.imagePreviewUrl} alt="profile-pic"/>
+                  </div>
+                  <p>Username</p>
+              </Link>
+          );
+      } else return null;
+  };
+}
 export default User;
