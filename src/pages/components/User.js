0f3343a898a038e0b9ac129494f288383145af28
avincent6 <vincent6@purdue.edu>
2019-03-23 17:07:47 -0400

    add default image load error fix

--- src/pages/components/User.js
+++ src/pages/components/User.js
@@ -1,32 +1,35 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import api from '../../utils/api';
+import edit from '../../assets/images/edit-icon.png';
 
 class User extends Component {
   constructor(props) {
       super(props);
       this.state = {
-        file: '',
-        imagePreviewUrl: '',
+        imagePreviewUrl: null
       }
+      this.getProfilePicture = this.getProfilePicture.bind(this);
   }
   componentDidMount() {
     this.getProfilePicture();
   }
   async getProfilePicture() {
-    const response = await api.Get('/profilepicture/');
-    console.log("response", response);
-    if (response.status === 200) {
-      // this.setState({ imagePreviewUrl: response.profile.avatar.url });
-      console.log("success!");
-    }
+      const response = await api.Get('/profilepicture/');
+      if (response.status === 200) {
+        const url = `https://marcopoloinvestment.club${response.data}`;
+        this.setState({ imagePreviewUrl: url });
+      }
+  }
+  addDefaultSrc(ev) {
+    ev.target.src =`${edit}`;
   }
   render() {
       if (this.props.isAuthenticated === "true") {
           return (
               <Link className="user" to='/settings'>
                   <div className="profile-nav">
-                      <img className="profile-pic" src={this.state.imagePreviewUrl} alt="profile-pic"/>
+                      {this.state.imagePreviewUrl && <img className="profile-pic" src={this.state.imagePreviewUrl} alt="profile-pic" onError={this.addDefaultSrc}/>}
                   </div>
                   <p>Username</p>
               </Link>
