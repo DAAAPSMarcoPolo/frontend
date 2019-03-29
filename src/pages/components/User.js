1463bb82fac39322485efaff5a051ff02a272d8a
avincent6 <vincent6@purdue.edu>
2019-03-29 11:13:15 -0400

    pull username in user componentthat sits in the top corner of the app

--- src/pages/components/User.js
+++ src/pages/components/User.js
@@ -7,12 +7,15 @@ class User extends Component {
   constructor(props) {
       super(props);
       this.state = {
-        imagePreviewUrl: null
+        imagePreviewUrl: null,
+        username: 'Username'
       }
       this.getProfilePicture = this.getProfilePicture.bind(this);
+      this.getUserInfo = this.getUserInfo.bind(this);
   }
   componentDidMount() {
     this.getProfilePicture();
+    this.getUserInfo();
   }
   async getProfilePicture() {
       const response = await api.Get('/profilepicture/');
@@ -21,6 +24,14 @@ class User extends Component {
         this.setState({ imagePreviewUrl: url });
       }
   }
+  getUserInfo = async () => {
+    const res = await api.Get('/api/user/settings/');
+    const { user } = res.data;
+    console.log('user', user);
+    if (user.username) {
+      this.setState({ username: user.username });
+    }
+  };
   addDefaultSrc(ev) {
     ev.target.src =`${edit}`;
   }
@@ -31,7 +42,7 @@ class User extends Component {
                   <div className="profile-nav">
                       {this.state.imagePreviewUrl && <img className="profile-pic" src={this.state.imagePreviewUrl} alt="profile-pic" onError={this.addDefaultSrc}/>}
                   </div>
-                  <p>Username</p>
+                  <p>{this.state.username}</p>
               </Link>
           );
       } else return null;
