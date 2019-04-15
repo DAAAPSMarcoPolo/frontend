21ceaf0e27d3db31b88aa156da4e31fa6b6ee430
avincent6 <vincent6@purdue.edu>
2019-04-15 13:30:19 -0400

    move the backtest form to a modal

--- src/pages/algorithm/BacktestForm.js
+++ src/pages/algorithm/BacktestForm.js
@@ -1,43 +1,43 @@
 import React from 'react';
 import DatePicker from 'react-datepicker';
 import ChooseUniverse from '../universe/ChooseUniverse';
+import ModalWrapper from '../components/ModalWrapper';
 import 'react-datepicker/dist/react-datepicker.css';
-import x from '../../assets/images/x-icon.png';
 
-const BacktestForm = ({ submitForm, exitForm, parent, error, handleSelectUniverse }) => {
+const BacktestForm = ({ submitForm, parent, error, handleSelectUniverse, showModal, toggleState, name }) => {
     return (
-        <div className="con rel">
-        <img
-            className="icon roster"
-            src={x}
-            alt="x-icon"
-            onClick={exitForm}
-        />
-          <h3>Start a new Backtest</h3>
-            <form onSubmit={submitForm}>
-                <div>
-                    <DatePicker
-                        selected={parent.state.startDate}
-                        onChange={parent.handleStartDateSelect}
-                        maxDate={new Date()}
-                        placeholderText="Start Date"
-                        dateFormat="yyyy-MM-dd"
-                        name="startDate"
-                    />
-                    <DatePicker
-                        selected={parent.state.endDate}
-                        onChange={parent.handleEndDateSelect}
-                        maxDate={new Date()}
-                        placeholderText="End Date"
-                        dateFormat="yyyy-MM-dd"
-                        name="endDate"
-                    />
-                    <ChooseUniverse selectUniverse={parent.handleSelectUniverse}/>
-                    <input type="text" placeholder="Initial Funds" name="initial_funds" required/>
-                </div>
-                <button type="submit">Submit</button>
-            </form>
-        </div>
+      <ModalWrapper
+            title="Start a new Backtest"
+            width={800}
+            showOk={true}
+            showModal={showModal}
+            toggleState={toggleState}
+            name={name}
+            >
+                <form onSubmit={submitForm}>
+                    <div>
+                        <DatePicker
+                            selected={parent.state.startDate}
+                            onChange={parent.handleStartDateSelect}
+                            maxDate={new Date()}
+                            placeholderText="Start Date"
+                            dateFormat="yyyy-MM-dd"
+                            name="startDate"
+                        />
+                        <DatePicker
+                            selected={parent.state.endDate}
+                            onChange={parent.handleEndDateSelect}
+                            maxDate={new Date()}
+                            placeholderText="End Date"
+                            dateFormat="yyyy-MM-dd"
+                            name="endDate"
+                        />
+                        <ChooseUniverse selectUniverse={parent.handleSelectUniverse}/>
+                        <input type="text" placeholder="Initial Funds" name="initial_funds" required/>
+                    </div>
+                    <button type="submit">Submit</button>
+                </form>
+        </ModalWrapper>
     );
 };
 
