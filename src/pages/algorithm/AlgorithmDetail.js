a701fa34dabf02e2e4949eba4e94b93804bb66a8
Pranay Shoroff <pshoroff@hotmail.com>
2019-04-23 10:46:23 -0400

    Merged dev into branch, resolved merge conflicts

--- src/pages/algorithm/AlgorithmDetail.js
+++ src/pages/algorithm/AlgorithmDetail.js
@@ -55,7 +55,7 @@ class AlgorithmDetail extends Component {
                 vote_denied: false,
                 user: false
             },
-            noBacktests: false
+            noBacktests: false,
             funds: 0
         };
         this.toggleBacktestForm = this.toggleBacktestForm.bind(this);
