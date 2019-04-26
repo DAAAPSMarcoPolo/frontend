51c21a670caf767422423f7e0372559e55a0adbd
dpiotti <dpiotti@purdue.edu>
2019-04-26 00:12:03 -0400

    fix random error with no live instance message

--- src/pages/algorithm/AlgorithmDetail.js
+++ src/pages/algorithm/AlgorithmDetail.js
@@ -239,10 +239,10 @@ class AlgorithmDetail extends Component {
         if (this.state.algo_details.live) {
             this.setState({ isLive: !this.state.isLive });
         } else {
-            this.setState({
-                error:
-                    'No live instances yet, Create a new live instance from a backtest!'
-            });
+            // this.setState({
+            //     error:
+            //         'No live instances yet, Create a new live instance from a backtest!'
+            // });
             setTimeout(() => {
                 this.setState({ error: null });
             }, 5000);
